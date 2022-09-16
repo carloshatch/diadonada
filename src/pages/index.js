a3916bf0562c51989924335a066f5c2bfa664db7
Carlos H Romano <chromano+carloshatch@gmail.com>
2022-09-16 11:13:24 -0300

    Attempt to improve lighthouse score with lazy YT embed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { LiteYoutubeEmbed } from 'react-lite-yt-embed';
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -44,7 +45,7 @@ const IndexPage = () => (
                         <hr className="mt-4" />
                         <div className="">
                             <div className="mb-2">Sobre a aus&ecirc;ncia de tudo:</div>
-<iframe className="rounded w-full h-64"src="https://www.youtube.com/embed/CCGT4NzClWo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            <LiteYoutubeEmbed id="CCGT4NzClWo" className="rounded w-full h-64" />
                         </div>
                     </div>
                 </div>
